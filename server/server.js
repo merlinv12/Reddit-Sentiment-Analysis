6f75845c18a86446f68d06d5efcbe8254daeb6ca
Anish Patel <anish@Anishs-MacBook.local>
2019-08-12 13:58:26 -0700

    post request to /api/username returns analyzed comments

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const { spawn, exec } = require("child_process");
+const controllers = require('./controllers/sentAnalysis.js')
+const bodyParser = require("body-parser");
 const PORT = 3333;
 
 const app = express();
-
+app.use(bodyParser());
 app.use(express.static(`${__dirname}/../public`));
 
-app.listen(PORT, console.log(`Server is now listening on ${PORT}`))
+app.post("/api/username/", (req, res) => {
+  controllers(req.body.username)
+  .then(data => res.status(202).send(data))
+});
+
+app.listen(PORT, console.log(`Server is now listening on ${PORT}`));
