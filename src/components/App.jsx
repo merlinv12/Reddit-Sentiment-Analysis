daa6e81b813ab4d8c8b897b1e88e56123901cd67
Anish Patel <anish@Anishs-MacBook.local>
2019-08-12 19:17:46 -0700

    added MUI styling entry for other components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import RedditComments from './RedditComments.jsx';
+import styled from 'styled-components';
 
+const FlexBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 class App extends React.Component{
-
     render(){
         return(
-            <div>Hello World!</div>
+            <FlexBox>
+                <RedditComments />
+            </FlexBox>
         )
     }
 }
