d09d0c86b782da3566a7e5fc4d37073edda6fd7a
Anish Patel <anish@Anishs-MacBook.local>
2019-08-13 12:41:58 -0700

    added route for put request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { spawn, exec } = require("child_process");
 const controllers = require('./controllers/sentAnalysis.js')
+const db = require('./database/db.js')
 const bodyParser = require("body-parser");
 const PORT = 3333;
 
@@ -10,7 +11,17 @@ app.use(express.static(`${__dirname}/../public`));
 
 app.post("/api/username/", (req, res) => {
   controllers(req.body.username)
+  .tap(data => db.insertCommentsFromUser(data)).then(console.log('Added to DB')).catch(err => console.log(err))
   .then(data => res.status(202).send(data))
+  .catch(err => console.log(err))
+
 });
+
+app.put("/api/comments/:id", (req, res) => {
+  console.log(req.body)
+  db.updateCommentUserInput(req.params.id, req.body)
+  .then(data => res.status(202).send(data))
+  .catch(err => console.log(err))
+}) 
 
 app.listen(PORT, console.log(`Server is now listening on ${PORT}`));
